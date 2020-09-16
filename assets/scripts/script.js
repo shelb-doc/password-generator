1ca17d74e1cae0c430d9d9c5c3709f0d90e6490d
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-09-15 21:33:13 -0400

    connected the css and js to html and added prompts for criteria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,11 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  var criteria = promptForCriteria();
+  // passwordText.value = password;
 
-  passwordText.value = password;
+}
 
+function promptForCriteria(){
+  var useLowercase = confirm("Include Lowercase Characters?");
+  alert("Include Lowercase Characters? " + useLowercase);
+
+  var useUppercase = confirm("Include Uppercase Characters?");
+  alert("Include Uppercase Characters? " + useUppercase);
+
+  var useNumbers = confirm("Include Numbers?");
+  alert("Include Numbers? " + useNumbers);
+
+  var useSpecialCharacters = confirm("Include Special Characters?");
+  alert("Include Special Characters? " + useSpecialCharacters);
 }
 
 // Add event listener to generate button
