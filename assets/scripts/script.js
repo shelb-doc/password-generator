0691f54fd933158f355f2096d076cd1f437648c6
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-09-17 22:09:54 -0400

    added comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,26 @@
-// Assignment Code
+// Assignment Code.
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
-  
+  // Find text area with id password.
   var passwordText = document.querySelector("#password");
+
+  // Capture User Criteria.
   var criteria = promptForCriteria();
 
+  // Use given Criteria to create a password.
   var password = generatePassword(criteria);
 
+  // Update text area with generated password.
   passwordText.value = password;
 
 }
 
+// Captures User Criteria for the Password and returns it in a dictionary.
 function promptForCriteria(){
+
+  // Declaring variables.
   var userInputs = {};
   userInputs.passwordLength = null;
   userInputs.useLowercase = null;
@@ -21,12 +28,17 @@ function promptForCriteria(){
   userInputs.useNumbers = null;
   userInputs.useSpecialCharacters = null;
 
+  /* Getting user input for Password Length,
+    ensuring that the length is between 8 and 128 characters and
+    stopping users from inputting anything but a number.
+  */
   while ((userInputs.passwordLength < 8) ||
         (userInputs.passwordLength > 128) ||
         (isNaN(userInputs.passwordLength))){
 
     userInputs.passwordLength = prompt ("How long should the password be?")
 
+    // Error message in case input doesn't meet criteria.
     if((userInputs.passwordLength < 8) ||
     (userInputs.passwordLength > 128) ||
     (isNaN(userInputs.passwordLength))){
@@ -34,6 +46,9 @@ function promptForCriteria(){
     }
   }
 
+/*  Getting input for Character types to use in the password,
+    making sure that the user selects at least one.
+*/
   while (!(userInputs.useLowercase) &&
         !(userInputs.useUppercase) &&
         !(userInputs.useNumbers) &&
@@ -46,6 +61,7 @@ function promptForCriteria(){
 
     userInputs.useSpecialCharacters = confirm("Include Special Characters?");
 
+    // Error in case no type is selected.
     if(!(userInputs.useLowercase) &&
         !(userInputs.useUppercase) &&
         !(userInputs.useNumbers) &&
@@ -62,26 +78,37 @@ function promptForCriteria(){
   return userInputs;
 }
 
+// Generates a Password using the provided dictionary and returns a string.
 function generatePassword(criteria){
+
+  // Declaring Variables.
   var options = "";
   var newPassword = "";
 
+  // Adding Lowercase characters to the options.
   if(criteria.useLowercase){
     options = options + "abcdefghijklmnopqrstuvwxyz"
   }
 
+  // Adding Uppercase characters to the options.
   if(criteria.useUppercase){
     options = options + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
 
+  // Adding Numbers characters to the options.
   if(criteria.useNumbers){
     options = options + "0123456789"
   }
 
+  // Adding Special characters to the options.
   if(criteria.useSpecialCharacters){
     options = options + "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   }
 
+  /* Generate the password by iterating the length provided by the user,
+    then generating a random number in between 0 and the length of our options,
+    and using that random index to return a character from the options.
+*/
   for (i = 1; i <= criteria.passwordLength; i++) {
     var index = Math.floor(Math.random() * options.length);
     newPassword = newPassword + options.charAt(index)
