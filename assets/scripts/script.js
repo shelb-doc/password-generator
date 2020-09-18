c4010de9d9a81dd0f7f465e0c31a4cfec162b4ec
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-09-17 20:27:22 -0400

    updated criteria function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,17 +11,31 @@ function writePassword() {
 }
 
 function promptForCriteria(){
+  var passwordLength = null;
+
+  while ((passwordLength < 8) ||
+        (passwordLength > 128) ||
+        (isNaN(passwordLength))){
+    passwordLength = prompt ("How long should the password be?")
+  }
+
   var useLowercase = confirm("Include Lowercase Characters?");
-  alert("Include Lowercase Characters? " + useLowercase);
+  // alert("Include Lowercase Characters? " + useLowercase);
 
   var useUppercase = confirm("Include Uppercase Characters?");
-  alert("Include Uppercase Characters? " + useUppercase);
+  // alert("Include Uppercase Characters? " + useUppercase);
 
   var useNumbers = confirm("Include Numbers?");
-  alert("Include Numbers? " + useNumbers);
+  // alert("Include Numbers? " + useNumbers);
 
   var useSpecialCharacters = confirm("Include Special Characters?");
-  alert("Include Special Characters? " + useSpecialCharacters);
+  // alert("Include Special Characters? " + useSpecialCharacters);
+
+  alert("How long should the password be? " + passwordLength + "\n" +
+        "Include Lowercase Characters? " + useLowercase + "\n" +
+        "Include Uppercase Characters? " + useUppercase + "\n" +
+        "Include Numbers? " + useNumbers + "\n" +
+        "Include Special Characters? " + useSpecialCharacters);
 }
 
 // Add event listener to generate button
